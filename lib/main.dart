05088e8d69fb3d5f5577e64409f2fc5b056bd1b2
soumistar <ismailatoure435@gmail.com>
2024-04-06 16:54:04 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  double? poids;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
+        child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+              title: Text(widget.title),
+            ),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  textAvecStyle(
+                      "Remplissez tous les champs pour obtenir votre besoin journalier en calorie"),
+                  const Card(
+                      elevation: 10,
+                      child: Column(
+                        children: <Widget>[
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+
+                            ],
+                          ),
+                          TextField(
+                            keyboardType: TextInputType.number,
+                            // onChanged: (String string="") {
+                            //   setState(() {
+                            //     poids = double.tryParse(string);
+                            //   });
+                            // }
+                            decoration: InputDecoration(
+                              labelText: "Entrez votre poids en kilos",
+                            ),
+                          ),
+                        ],
+                      ))
+                ],
+              ),
+            )));
+  }
+
+  Text textAvecStyle(String data,
+      {color = Colors.black, double fontSize = 15}) {
+    return Text(
+      data,
+      textAlign: TextAlign.center,
+      style: TextStyle(color: color, fontSize: fontSize),
+    );
+  }
+}
