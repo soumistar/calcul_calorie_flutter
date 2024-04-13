67be1785220b344743f926c999fd4c118ee0f4c5
soumistar <ismailatoure435@gmail.com>
2024-04-13 13:52:26 +0000

    Calcul et alertes

--- lib/main.dart
+++ lib/main.dart
@@ -31,14 +31,16 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int radioSelectionne = 0;
+  late int calorieBase;
+  late int calorieAvecActivite;
   double? poids;
   bool genre = false;
   double age = 0.0;
   double taille = 170.0;
   Map mapActipe = {
-    "Faible": 0,
-    "Modere": 1,
-    "Forte": 2,
+    0: "Faible",
+    1: "Modere",
+    2: "Forte",
   };
   @override
   Widget build(BuildContext context) {
@@ -57,7 +59,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   padding(),
                   textAvecStyle(
                       "Remplissez tous les champs pour obtenir votre besoin journalier en calorie"),
-                      padding(),
+                  padding(),
                   Card(
                       elevation: 10,
                       child: Column(
@@ -109,7 +111,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                 });
                               },
                               max: 215.0,
-                              min: 100.0),
+                              min: 0.0),
                           padding(),
                           TextField(
                             keyboardType: TextInputType.number,
@@ -123,12 +125,19 @@ class _MyHomePageState extends State<MyHomePage> {
                             ),
                           ),
                           padding(),
-                          textAvecStyle("Quelle est votre activité sportive:", color: setColor()),
+                          textAvecStyle("Quelle est votre activité sportive:",
+                              color: setColor()),
                           padding(),
                           rowRadio(),
                           padding(),
                         ],
-                      ))
+                      )),
+                  padding(),
+                  ElevatedButton(
+                      onPressed: calculerNomvreDeCalorie,
+                      style:
+                          ElevatedButton.styleFrom(backgroundColor: setColor()),
+                      child: textAvecStyle("Calculer", color: Colors.white)),
                 ],
               ),
             )));
@@ -146,30 +155,29 @@ class _MyHomePageState extends State<MyHomePage> {
     return const Padding(padding: EdgeInsets.only(top: 20));
   }
 
-  Row rowRadio(){
+  Row rowRadio() {
     List<Widget> l = [];
     mapActipe.forEach((key, value) {
       Column colonne = Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Radio(
-            activeColor: setColor(),
-            value: value, 
-          groupValue: radioSelectionne, 
-          onChanged:  (dynamic i){
-            setState(() {
-              radioSelectionne = i;
-            });
-          } ),
-          textAvecStyle(key, color: setColor()),
-
+              activeColor: setColor(),
+              value: key,
+              groupValue: radioSelectionne,
+              onChanged: (dynamic i) {
+                setState(() {
+                  radioSelectionne = i;
+                });
+              }),
+          textAvecStyle(value, color: setColor()),
         ],
       );
       l.add(colonne);
     });
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children : l, 
+      children: l,
     );
   }
 
@@ -188,7 +196,7 @@ class _MyHomePageState extends State<MyHomePage> {
         age = ans;
       });
     }
-    
+
     return choix; // Ajoutez cette ligne pour renvoyer la valeur obtenue
   }
 
@@ -199,5 +207,84 @@ class _MyHomePageState extends State<MyHomePage> {
       textAlign: TextAlign.center,
       style: TextStyle(color: color, fontSize: fontSize),
     );
+  }
+
+  void calculerNomvreDeCalorie() {
+    if (age != 0 && poids != 0 && radioSelectionne != 0) {
+      if (genre) {
+        calorieBase =
+            (66.4730 + (13.7516 * poids!) + (5.0033 * taille) - (6.7550 * age))
+                .toInt();
+      } else {
+        calorieBase =
+            (655.0955 + (9.5634 * poids!) + (1.8496 * taille) - (4.6756 * age))
+                .toInt();
+      }
+      switch (radioSelectionne) {
+        case 0:
+          calorieAvecActivite = (calorieBase * 1.2).toInt();
+          break;
+        case 1:
+          calorieAvecActivite = (calorieBase * 1.5).toInt();
+          break;
+        case 2:
+          calorieAvecActivite = (calorieBase * 1.8).toInt();
+          break;
+        default:
+          calorieAvecActivite = calorieBase;
+          break;
+      }
+
+      setState(() {
+        dialogue();
+      });
+    } else {
+      alerte();
+    }
+  }
+
+  Future<Null> dialogue() async {
+    return showDialog(
+        context: context,
+        builder: (BuildContext buildContext) {
+          return SimpleDialog(
+            title:
+                textAvecStyle("Votre bessoin en calories", color: setColor()),
+            contentPadding: EdgeInsets.all(15),
+            children: <Widget>[
+              padding(),
+              textAvecStyle("Votre bessoin en base est de : $calorieBase"),
+              padding(),
+              textAvecStyle(
+                  "Votre bessoin en activité sportive est de : $calorieAvecActivite"),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(buildContext).pop();
+                },
+                child: textAvecStyle("OK", color: Colors.white),
+                style: ElevatedButton.styleFrom(backgroundColor: setColor()),
+              )
+            ],
+          );
+        });
+  }
+
+  Future<Null> alerte() async {
+    return showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (BuildContext buildContext) {
+          return AlertDialog(
+            title: textAvecStyle("Error"),
+            content: textAvecStyle("Tous les champs ne sont pas remplies"),
+            actions: <Widget>[
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(buildContext).pop();
+                  },
+                  child: textAvecStyle("OK", color: Colors.red))
+            ],
+          );
+        });
   }
 }
